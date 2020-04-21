6a85fc28df9ae083a37a3b24f09619de470ef820
Suyash <suyash1629@gmail.com>
2020-04-21 18:35:54 -0400

    barebones code for pubsub client connection with heartbeat feature to keep the connection alive

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from webSocketClient import WebSocketClient
+
+if __name__ == '__main__':
+    # Creating client object
+    client = WebSocketClient()
+    loop = asyncio.get_event_loop()
+    # Start connection and get client connection protocol
+    connection = loop.run_until_complete(client.connect())
+    # Start listener and heartbeat
+    tasks = [
+        asyncio.ensure_future(client.heartbeat(connection)),
+        asyncio.ensure_future(client.receiveMessage(connection)),
+    ]
+
+    loop.run_until_complete(asyncio.wait(tasks))
---
+++ webSocketClient.py
@@ -0,0 +1,61 @@
+import websockets
+import asyncio
+import uuid
+import json
+
+class WebSocketClient():
+
+    def __init__(self):
+        self.topics = ["channel-points-channel-v1.<ChannelID>"]
+        self.auth_token = "<Auth_Token>"
+        pass
+
+    async def connect(self):
+        '''
+           Connecting to webSocket server
+           websockets.client.connect returns a WebSocketClientProtocol, which is used to send and receive messages
+        '''
+        self.connection = await websockets.client.connect('wss://pubsub-edge.twitch.tv')
+        if self.connection.open:
+            print('Connection stablished. Client correcly connected')
+            # Send greeting
+            message = {"type": "LISTEN", "nonce": str(self.generate_nonce()), "data":{"topics": self.topics, "auth_token": self.auth_token}}
+            json_message = json.dumps(message)
+            await self.sendMessage(json_message)
+            return self.connection
+
+    def generate_nonce(self):
+        '''Generate pseudo-random number and seconds since epoch (UTC).'''
+        nonce = uuid.uuid1()
+        oauth_nonce = nonce.hex
+        return oauth_nonce
+
+    async def sendMessage(self, message):
+        '''Sending message to webSocket server'''
+        await self.connection.send(message)
+
+    async def receiveMessage(self, connection):
+        '''Receiving all server messages and handling them'''
+        while True:
+            try:
+                message = await connection.recv()
+                print('Received message from server: ' + str(message))
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection with server closed')
+                break
+
+    async def heartbeat(self, connection):
+        '''
+        Sending heartbeat to server every 1 minutes
+        Ping - pong messages to verify/keep connection is alive
+        '''
+        while True:
+            try:
+                data_set = {"type": "PING"}
+                json_request = json.dumps(data_set)
+                print(json_request)
+                await connection.send(json_request)
+                await asyncio.sleep(60)
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection with server closed')
+                break
